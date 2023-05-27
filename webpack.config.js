7a511d4665bb82ce64155bc848e29983f132ed92
M. Canul <canulmauricio03@gmail.com>
2023-05-27 12:33:35 -0400

    Set up complete :D

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map'
   entry: './src/index.ts',
   module: {
     rules: [
@@ -11,8 +13,11 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   output: {
-    publicPath: 'public',
+    publicPath: '/public/',
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
